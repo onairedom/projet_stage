ee4ab2800a01f5f49adde98341706642612af045
oneraidom <moderiano.benoit@gmail.com>
2016-04-15 12:33:16 +0200

    fonction tableau semaine presque terminé

--- Affichage.py
+++ Affichage.py
@@ -143,54 +143,127 @@ plt.ylabel(u"Nombre d'heures")
 plt.title("Nombre d'heures par matière sur l'année")
 plt.text(600,500,'STCE=SOUTENANCE EW=Eval Wims')# a travailler
 plt.show()
-
 """
+
 ###########################################création tableau pour toutes les semaines####################################################
-Sem=[]
-k=0
-longueur=len(recap) #pr créer un tableau ayant la semaine suivi de chaque matière elle même suivi du nombre d'heure par semaine
-l=longueur*2 # on mutliplie la longueur par 2 a cause du nombre d'heure qui apparait derrière le tableau
-for i in lst:
-	if lst[k][7] not in Sem:
-		Sem.append(lst[k][7])
-	k+=1
-nb=len(Sem)
+
+l=53 # on créé une variable qui rpz le nb de semaine plus 1pour le nombre de matière
 
 Semaine=[]
-for i in range(l*nb):
+for i in range(l):
 	Semaine.append([]);
 
 
+Semaine[0].append(0)
+for i in recap:	# On créé le nb de colonnes qui correspondent au nombre de matière plus 1
+	Semaine[0].append(i)
 
-# k=0
-# m=0
-# if k%(l+1)==0:		# Dans l'exemple on insère le nom de la semaine modulo nb de matière *2+1
-# 	for j in Sem:
-# 		if j not in Semaine:   #vérifie si la semaine existe dans le tableau existe et laisse place pour les matières et heures
-# 			Semaine[k]=j
-# if  k%2==0 and k!=0:	#Tous les nb pair du tableau correspondront aux matières
-# 	for n in recap: # parcours tableau matière
-# 		Semaine[k]=n
-# if  k%3==0 and k!=0: # Tous les modulos de 3 correspondront aux nombres de seconde par matières de chaque semaines
-# 	for m in time4:
-# 		Semaine[k]=m
+nb_mat=len(recap)
+k=0;
+for j in Semaine:		#On créé le nombre des semaines dans la colonne 0 du tableau
+	if k>0:
+		Semaine[k].append(k)
+		for m in range(nb_mat):		#On ajoute les colonnes vide à chaque ligne
+			Semaine[k].append([])
+	k+=1
+
+print(Semaine[0])
+for j in lst:		#On va chercher le nombre d'heure par matière par semaine pour compléter ce nouveau tableau
+	s=j[7]
+	m=j[0]
+	Hour=','.join(j[6])  #Sépare  heure pour créer Heure avec datetime
+	H=Hour[0]
+	if Hour[2].isnumeric()==True:
+		H=Hour[0]+Hour[2]
+	if Hour[6].isnumeric()==True and Hour[4].isnumeric()==True:
+		M=Hour[4]+Hour[6]
+	else:
+		M=Hour[6]+Hour[8]
+	H=int(H)
+	M=int(M)
+	t1=datetime.timedelta(0,(3600*H+60*M))
+	time=t1.total_seconds()
+	time2=time/3600
+	if m=='STCE':
+		m='SOUTENANCE'
+	elif m=='EW':
+		m='Eval Wims'
+	if m in Semaine[0]:
+		index=Semaine[0].index(m)
+	Semaine[s][index]=time2
+
+
+###############################################création 2ème histogramme pour nb heure matière sur toute l'année
+
+for i in range(nb_mat): #car on commence a 0 et pour faire correspondre les numéros de matières avec les heures
+	 globals()['y%s' % i] = [] # créé des tableau avec des noms comportznt un chiffre de 0 à nb de matière
+
 	
-# k+=1
+
+nb_heure=[];matiere=[];x=[];
+m=0;n=0;k=0;
+for i in Semaine: #Parcours tableau semaine
+	for j in i:	#parcours chaque ligne avec nombre d'heure ont le meme indice que dans le tableau recap des matière
+		if m==8:
+			m=0;
+		if type(j)==float :
+			globals()['y%s' % m].append(j) # utilise tableau ayant des noms de 0 à nb_matière et ajoute le nombre d'heure a l'intérieur
+		m+=1;	
+			
+print("c'est moi",recap)
+print(y0)
+print(y1)
+print(y2)
+print(y3)
+print(y4)
+print(y6)
+print(y7)
+
+for j in Semaine:
+	x.append(k)
+	
+	k=k+1
+	
+print(x)
 
 
-#Améliorer ça et le faire dans le grand tableau lst pour que cela marche
-k=0;j=0;
-for i in lst:
-	Semaine[k]=lst[j][7]
-	S=Semaine[k]
-	for S in lst:
-		j+=1
-		k+=1
-		Semaine[k]=lst[j][0]
-		k+=1
-		Semaine[k]=lst[j][6]
-		k+=1
-	j+=1
 
+plt.hist([for i in range(nb_mat):globals()['y%s' % i],], range = (1, len(x)), bins = len(x), color = ['yellow','green','blue','orange'],
+            edgecolor = 'red',histtype = 'barstacked',align='left')
+plt.xlabel('valeurs')
+plt.ylabel('nombres')
+plt.title('Exemple d\' histogramme simple')
+plt.show()
 
+"""
+plt.bar(x, nb_heure, align='center', edgecolor = 'red', hatch = '/')  # ces deux lignes permettent de mettre des string dans un histogrammes plutot que des entiers
+# plt.xticks(x, matiere) # x est une liste composé du nombre d'éléments qe l'on veut mettre en horizontal
+plt.grid(True) # celà sert à mettre une grille dans le diagramme
+plt.xlabel('Semaines') 
+plt.ylabel(u"Nombre d'heures")
+plt.title("Nombre d'heures par matière sur l'année")
+plt.text(600,500,'STCE=SOUTENANCE EW=Eval Wims')# a travailler
+plt.show()"""
 
+# x1 = [1, 4, 2, 3, 4, 4, 4, 4, 4, 5, 5]
+# x2 = [1, 4, 1, 2, 2, 3, 3, 3, 3, 4, 5, 5, 5]
+# bins = [x + 0.5 for x in range(0, 6)]
+# plt.hist(x1, bins = bins, color = 'yellow',edgecolor = 'red', hatch = '/', label = 'x1')
+# # plt.hist(x2, bins = bins, color = 'green', alpha = 0.5,edgecolor = 'blue', hatch = '/', label = 'x2')
+# # plt.ylabel('valeurs')
+# plt.xlabel('nombres')
+# plt.title('superpose')
+# plt.legend()
+# plt.show()
+
+"""
+x = [1, 1, 1, 3, 3, 4, 2, 2, 4, 5,4]
+x1=[1,1,2,2,3,4,5]
+x2=[1,1,2,2,2,3,4,4,5,5,5]
+x3=[1,2,2,2,2,3,4,4,5,5,5]
+plt.hist([x,x1,x2,x3], range = (1, 5), bins = 5, color = ['yellow','green','blue','orange'],
+            edgecolor = 'red',histtype = 'barstacked',align='left')
+plt.xlabel('valeurs')
+plt.ylabel('nombres')
+plt.title('Exemple d\' histogramme simple')
+plt.show()"""
